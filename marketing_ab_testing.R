a5a228f45b2c3bec1f6013628f3fd02fe4b23106
rsm-sal072 <sal072@ucsd.edu>
2024-04-12 02:45:55 -0700

    04122024

---
+++ marketing_ab_testing.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(rmarkdown)
+library(shinyjs)
+
+ui <- fluidPage(
+  id = "main_nav",
+  title = (""),
+  tabPanel("",
+           fluidPage(
+             uiOutput("chapter")))
+)
+
+server <- function(input, output, session) {
+  output$chapter <- renderUI({
+    file_name <- "A Replication of Karlan and List (2007).html" 
+    if (!is.null(file_name)) {
+      tags$iframe(src = file_name, style = "width:100%; height:800px;")
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
